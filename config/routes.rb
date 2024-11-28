0629c81852c7de334bc1782c2fdd9369e38e52d1
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-11-28 11:09:31 +0000

    4-7_20241128_2[Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/top' => 'homes#top'
   resources :meshiterros
-
+  get 'homes/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
