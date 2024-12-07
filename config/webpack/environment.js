7048952b460e2b700db63c8a4e666d1c28919e54
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-07 08:57:52 +0000

    4-24_20241207[Finish]

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+onst webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
